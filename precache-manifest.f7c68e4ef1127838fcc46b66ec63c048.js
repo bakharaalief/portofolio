fa7c971e7c9b7ec2d1064e7b31e0e57d31106e21
bakharaalief <ara.bakhara12@gmail.com>
2019-09-01 11:52:18 +0700

    Updates

---
+++ precache-manifest.f7c68e4ef1127838fcc46b66ec63c048.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf2985d4f3d5f797b9fcf1da20ba10b",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "0d0980834cdef637516f",
+    "url": "/portofolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f1d946c8adfd78338488",
+    "url": "/portofolio/static/css/main.85a01483.chunk.css"
+  },
+  {
+    "revision": "0d0980834cdef637516f",
+    "url": "/portofolio/static/js/2.84bdcca6.chunk.js"
+  },
+  {
+    "revision": "f1d946c8adfd78338488",
+    "url": "/portofolio/static/js/main.f86b8abf.chunk.js"
+  },
+  {
+    "revision": "b4d97782048388af1bdb",
+    "url": "/portofolio/static/js/runtime~main.4f067f3c.js"
+  },
+  {
+    "revision": "aa0140a872db48e071534ae558d91d38",
+    "url": "/portofolio/static/media/1523342418956 (1).aa0140a8.jpg"
+  },
+  {
+    "revision": "2a652ced1e0cbacb2708a1d743dce3f4",
+    "url": "/portofolio/static/media/simplecounter.2a652ced.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bakharaalief/portofolio/precache-manifest.de0c1027bbf347ca9282117fa30ea5bc.js"
+  "/portofolio/precache-manifest.f7c68e4ef1127838fcc46b66ec63c048.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bakharaalief/portofolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portofolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
